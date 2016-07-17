789fed29dd7d74751794cf53cd4360976141d0e7
Cobrand <cobrandw@gmail.com>
2016-07-17 20:24:15 +0200

    parsing part 3 : parsing frm file

--- src/subtitles/sub.rs
+++ src/subtitles/sub.rs
@@ -19,6 +19,8 @@ pub struct Subtitles {
 
 impl Subtitles {
     pub fn load_from_lyr_frm<P:AsRef<Path>>(lyr:P,frm:P) -> Result<Subtitles,String> {
+        let frm : &Path = frm.as_ref();
+        let lyr : &Path = lyr.as_ref();
         let mut subtitles = Subtitles::default();
         let lyr_file = try!(File::open(lyr).map_err(|e| e.description().to_string() ));
         let frm_file = try!(File::open(frm).map_err(|e| e.description().to_string() ));
@@ -27,7 +29,8 @@ impl Subtitles {
         for (line_number,lyr_line) in lyr_file.lines().enumerate() {
             let lyr_line = try!(
                 lyr_line.map_err(|e|
-                    format!("IoError when reading file at line {} : '{}'",
+                    format!("IoError when reading {} at line {} : '{}'",
+                            lyr.display(),
                             line_number,
                             e.description())
                 )
@@ -51,6 +54,55 @@ impl Subtitles {
                 };
                 subtitles.sentences.push(sentence);
             };
+        }
+        let mut frames : Vec<(u32,u32)> = vec!();
+        for (line_number,frm_line) in frm_file.lines().enumerate() {
+            let frm_line = try!(
+                frm_line.map_err(|e|
+                    format!("IoError when reading {} at line {} : '{}'",
+                            frm.display(),
+                            line_number,
+                            e.description())
+                )
+            );
+            if !frm_line.trim().is_empty() {
+                let line_frames : Result<Vec<_>,_> = frm_line.split(' ')
+                                                     .map(|s| s.parse::<u32>())
+                                                     .collect();
+                let begin_end = line_frames.map_err(|e| {
+                    format!("{}",e)
+                }).and_then(|line_frames| {
+                    match (line_frames.get(0),line_frames.get(1),line_frames.get(2)) {
+                        (Some(&begin),Some(&end),None) => {
+                            Ok((begin,end))
+                        },
+                        (None,_,_) | (_,None,_) => {
+                            Err(format!("Error while parsing frm file '{}' at line {}, not enough values",frm.display(),line_number))
+                        },
+                        (_,_,Some(_)) => {
+                            Err(format!("Error while parsing frm file '{}' at line {}, too many values",frm.display(),line_number))
+                        }
+                    }
+                });
+                let begin_end = try!(begin_end);
+                frames.push(begin_end);
+            } else {
+                warn!("empty line {} in frm file '{}'",line_number,frm.display());
+            }
+        };
+        let mut frame_iter = frames.iter();
+        'sentences: for sentence in subtitles.sentences.iter_mut() {
+            for syllable in sentence.syllables.iter_mut() {
+                match frame_iter.next() {
+                    Some(&(begin,end)) => {
+                        syllable.begin = begin ;
+                        syllable.end = end ;
+                    },
+                    None => {
+                        break 'sentences;
+                    }
+                }
+            }
         }
         Ok(subtitles)
     }
