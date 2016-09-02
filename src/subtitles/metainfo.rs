4efe5a4d4384cf07b0777b4b50d8ee16f3393fe2
Cobrand <cobrandw@gmail.com>
2016-09-02 23:51:39 +0200

    Changed metainfo.rs
    Changed so it better stringifies to JSON and YAML

--- src/subtitles/metainfo.rs
+++ src/subtitles/metainfo.rs
@@ -12,14 +12,10 @@ pub enum Language {
 #[allow(dead_code)]
 #[derive(Debug,Clone,Serialize,Deserialize)]
 pub enum MusicType {
-    Opening { number: i32 },
-    Ending { number: i32 },
+    Opening,
+    Ending,
     Insert,
-    Other {
-        kind: String,
-        #[serde(skip_serializing_if="Option::is_none")]
-        number: Option<i32>,
-    },
+    Other(String),
 }
 
 #[allow(dead_code)]
@@ -28,7 +24,8 @@ pub enum MediaType {
     Anime,
     Movie,
     VideoGame,
-    Other { kind: String },
+    AMV,
+    Other(String),
 }
 
 #[derive(Debug,Clone,Default,Serialize,Deserialize)]
@@ -36,56 +33,27 @@ pub struct MetaInfo {
     #[serde(skip_serializing_if="Option::is_none")]
     pub artist: Option<String>,
     #[serde(skip_serializing_if="Option::is_none")]
-    pub origin: Option<String>, // name of anime / movie / video game
+    /// name of anime / movie / video game
+    pub media_title: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub media_alt_titles: Option<Vec<String>>,
     #[serde(skip_serializing_if="Option::is_none")]
     pub song_name: Option<String>,
     #[serde(skip_serializing_if="Option::is_none")]
     pub music_type: Option<MusicType>,
     #[serde(skip_serializing_if="Option::is_none")]
+    /// 5 of ED5 for instance
+    pub music_number: Option<u32>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    /// "v2" of ED5v2
+    pub version: Option<String>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    /// Anime / Movie / ...
     pub media_type: Option<MediaType>,
     #[serde(skip_serializing_if="Option::is_none")]
     pub language: Option<Language>,
-}
-
-impl MetaInfo {
-    #[allow(dead_code)]
-    pub fn new() -> MetaInfo {
-        MetaInfo::default()
-    }
-
-    #[allow(dead_code)]
-    pub fn with_artist(mut self, artist: String) -> MetaInfo {
-        self.artist = Some(artist);
-        self
-    }
-
-    #[allow(dead_code)]
-    pub fn with_origin(mut self, origin: String) -> MetaInfo {
-        self.origin = Some(origin);
-        self
-    }
-
-    #[allow(dead_code)]
-    pub fn with_song_name(mut self, song_name: String) -> MetaInfo {
-        self.song_name = Some(song_name);
-        self
-    }
-
-    #[allow(dead_code)]
-    pub fn with_music_type(mut self, music_type: MusicType) -> MetaInfo {
-        self.music_type = Some(music_type);
-        self
-    }
-
-    #[allow(dead_code)]
-    pub fn with_media_type(mut self, media_type: MediaType) -> MetaInfo {
-        self.media_type = Some(media_type);
-        self
-    }
-
-    #[allow(dead_code)]
-    pub fn with_language(mut self, language: Language) -> MetaInfo {
-        self.language = Some(language);
-        self
-    }
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub year: Option<u32>,
+    #[serde(skip_serializing_if="Option::is_none")]
+    pub creditless: Option<bool>,
 }
